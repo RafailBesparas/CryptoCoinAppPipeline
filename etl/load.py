7397137584787060f1d32c654b6a6d6e72ff2773
RafailBesparas <117984967+RafailBesparas@users.noreply.github.com>
2024-12-27 14:39:48 +0200

    Delete etl directory

--- etl/load.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-
-def load_data(df, db_url, table_name='crypto_prices'):
-    print("Loading data into PostgreSQL Local Database")
-
-    engine = create_engine(db_url)
-    df.to_sql(table_name, engine, if_exists='append', index=False)
-    print("Data successfully loaded.")
